43902aac9addd2bd0b2d66f8a018fd011f0a536c
Bejetto <bejetto98@gmail.com>
2025-03-07 10:14:22 +0900

    v0.1.31] feat. VersionUI 추가

--- Assets/_Script/Scenes/SceneStart.cs
+++ Assets/_Script/Scenes/SceneStart.cs
@@ -1,8 +1,21 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneStart : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI tmVersion;
+
+    void OnValidate()
+    {
+        if (tmVersion != null)
+        {
+            tmVersion.text = $"v{Application.version}";
+        }    
+            
+
+    }
+
     public void TapToStart()
     {
         //Debug.Log("탭 투 스타트");
